4c1f01600274d6310fe6bd235fcb40704425a044
Ahmed Maged <amaged606@gmail.com>
2026-04-28 00:09:59 +0300

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,17 @@
+
+--create the Database--
+
+create database DataWarehouse
+
+--using it--
+  
+use DataWarehouse
+
+--Create 3 schemas--
+
+  
+CREATE SCHEMA Bronze
+go
+CREATE SCHEMA Silver
+go
+CREATE SCHEMA Gold
